e4ddc0b25b3eef79156a4f8e551a788177231736
viacheslove <rrassollnik@gmail.com>
2021-06-19 13:48:42 +0300

    add
    add

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $p = 'Welcome!';
+?>
+
+<?php
+    $name = 'Viacheslav';
+    $surname = 'Hacker-7';
+    $city = 'Saint-Petersburg';
+    $age = 99;
+?>
+
+<?php
+    include 'main.php';
+?>
---
+++ logo.inc.php
@@ -0,0 +1,3 @@
+<div class="logo">
+    <a href="/mysite/"><img src="img/1.png" alt="php"></a>
+</div>
